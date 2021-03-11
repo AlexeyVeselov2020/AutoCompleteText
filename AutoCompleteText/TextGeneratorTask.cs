d03cc5ffa33d48bc8b6646fd19af27200526d714
Alexey Veselov <alexeyveselov2000@gmail.com>
2021-03-11 10:57:35 +0300

    KekW

--- AutoCompleteText/TextGeneratorTask.cs
+++ AutoCompleteText/TextGeneratorTask.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace TextAnalysis
 {
@@ -9,6 +11,34 @@ namespace TextAnalysis
             string phraseBeginning,
             int wordsCount)
         {
+            StringBuilder newstr = new StringBuilder();
+            string[] words = phraseBeginning.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            if (words.Length>1)
+            {
+                if (nextWords.ContainsKey(phraseBeginning))
+                {
+                    newstr.AppendFormat("{0} {1}", phraseBeginning, nextWords[phraseBeginning]);
+                    phraseBeginning = ContinuePhrase(nextWords, newstr.ToString(), wordsCount - 1);
+                }
+                else
+                    if (nextWords.ContainsKey(words[1]))
+                    {
+                        newstr.AppendFormat("{0} {1}", phraseBeginning, nextWords[words[words.Length-1]]);
+                        phraseBeginning = ContinuePhrase(nextWords, words[words.Length - 1], wordsCount - 1);
+                    }
+                else
+                    return phraseBeginning;
+            }
+            else
+            {
+                if (nextWords.ContainsKey(words[1]))
+                {
+                    newstr.AppendFormat("{0} {1}", phraseBeginning, nextWords[words[words.Length - 1]]);
+                    phraseBeginning = ContinuePhrase(nextWords, words[words.Length - 1], wordsCount - 1);
+                }
+                else
+                    return phraseBeginning;
+            }
             return phraseBeginning;
         }
     }

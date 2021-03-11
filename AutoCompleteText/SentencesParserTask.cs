af2a3582d2d4932b789f11f6b5766aec0b092ac9
Skorodumov <Skorodumov@PC403-03>
2021-03-11 10:36:37 +0300

    parser

--- AutoCompleteText/SentencesParserTask.cs
+++ AutoCompleteText/SentencesParserTask.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace TextAnalysis
 {
@@ -7,7 +9,40 @@ namespace TextAnalysis
         public static List<List<string>> ParseSentences(string text)
         {
             var sentencesList = new List<List<string>>();
-            var Sentence = new List<string>();
+            var sentence = new List<string>();
+            string _text = text;
+            //using (var sr = new StreamReader("HarryPotterText.txt"))
+            //{
+            //     _text = sr.ReadToEnd();
+            //}
+            var sb = new StringBuilder();
+            for (int i = 0; i < _text.Length; i++)
+            {
+                if (char.IsLetter(_text[i]) || _text[i] == '\'')
+                    sb.Append(_text[i]);
+                else
+                {
+                    if (sb.Length > 0)
+                    {
+                        sentence.Add(sb.ToString().ToLower());
+                        sb.Clear();
+                    }
+                    if (_text[i] == '.' || _text[i] == '!' || _text[i] == '?' || _text[i] == ';' || _text[i] == ':' || _text[i] == '(' || _text[i] == ')')
+                    {
+                        if (sentence.Count > 0)
+                        {
+                            sentencesList.Add(new List<string>(sentence));
+                            sentence.Clear();
+                        }
+                    }
+                }
+            }
+            if (sb.Length > 0)
+            {
+                sentence.Add(sb.ToString().ToLower());
+                sentencesList.Add(sentence);
+            }
+
             return sentencesList;
         }
     }
